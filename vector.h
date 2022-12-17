0c003f3c24da937a7edf5175c4081a7ceac5bc00
tagoras <pvaitkeviciuz@gmail.com>
2022-12-17 22:12:55 +0200

    Added member function declarations

---
+++ vector.h
@@ -0,0 +1,73 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <memory>
+
+namespace custom{
+
+template<typename T>
+class Vector{
+    public:
+        T* start{nullptr};
+        T* end{nullptr};
+        int length{};
+
+        // Copy control members
+        Vector() = default;
+        Vector(std::initializer_list<T>);
+        Vector(const Vector<T> &);
+        operator=(std::initializer_list<T>);
+        operator=(const Vector<T> &);
+        ~Vector();
+        Vector(const Vector<T> &&);
+        operator=(const Vector<T> &&);
+
+        //Access Functions
+        T& at(int) const;
+        T& operator[](int) const;
+        T& front() const;
+        T& back() const;
+
+        // Capacity Functions
+        bool empty() const;
+        int size() const;
+        int max_size() const;
+        void reserve();
+        void shrink_to_fit();
+
+        //Modifiers
+        void clear();
+
+        void insert(const T &);
+        void insert(std::initializer_list<T> elements);
+        
+        T erase(const T &);
+        T erase(int pos);
+        T erase(int start, int end);
+
+        void push_back(const T &);
+        void push_back(std::initializer_list<T> elements);
+        void push_back(const Vector<T> &other);
+
+        T pop_back();
+        void resize();
+
+        void swap(const Vector<T> other);
+};
+
+template <typename T>
+Vector<T>::Vector(std::initializer_list<T> elements){
+    length = elements.size();
+    start = new T[length * 2];
+
+    T* copy_start = start;
+    for(T element : elements)
+    {
+        *copy_start = element;
+        ++copy_start;
+    }
+}
+
+}
+
+#endif
