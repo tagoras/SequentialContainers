79da64c41e7d3c0d4c0d95990ac4a4fe8dbddb4d
tagoras <pvaitkeviciuz@gmail.com>
2022-12-19 21:33:46 +0200

    Testing changes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 int main()
 {
     custom::Vector<int> numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    custom::Vector<int> numbers2 = numbers;
+
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        std::cout << numbers[i] << " ";
+        //std::cout << numbers.at(i) << " ";
+    }
 
     return 0;
 }
