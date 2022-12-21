186b67b5225ee57529c4df983f5a010e407a9775
tagoras <pvaitkeviciuz@gmail.com>
2022-12-21 18:03:01 +0200

    Testing code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ int main()
     custom::Vector<int> numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     custom::Vector<int> numbers2{std::move(numbers)};
     
-    std::cout << numbers2 << std::endl;
+    for(int element : numbers2)
+    {
+        std::cout << element << " ";
+    }
 
     
 
