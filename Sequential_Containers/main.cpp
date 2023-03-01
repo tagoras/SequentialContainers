1e42b44bef7ae90515e211393c4086b6fcba9e56
tagoras <pvaitkeviciuz@gmail.com>
2023-03-01 21:00:17 +0000

    Added some unit testing

--- Sequential_Containers/main.cpp
+++ Sequential_Containers/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include "Vector.h"
+#include <vector>
 
-int main()
+// Static Linkage of Boost.test
+#define BOOST_TEST_MODULE Vector_Testing
+#include <boost/test/unit_test.hpp>
+
+custom::Vector<int> test_vector{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+BOOST_AUTO_TEST_CASE(test_vector_default_constructor)
 {
-    std::cout << "Hello World!\n";
+	custom::Vector<int> vec{};
+	BOOST_TEST(vec.size() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_constructor)
+{
+	custom::Vector<int> vec{ 1,2,3,4,5,6,7,8,9,10 };
+	BOOST_TEST(vec.size() == 10);
+}
+
+BOOST_AUTO_TEST_CASE(test_pop_back)
+{
+	custom::Vector<int> vec{ 1,2,3,4,5,6,7,8,9,10 };
+	vec.pop_back();
+	BOOST_TEST(vec.size() == 9);
 }
 
